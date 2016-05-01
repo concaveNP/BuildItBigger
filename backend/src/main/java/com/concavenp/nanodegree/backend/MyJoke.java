e98f0980c6ba08640e88135b4f3b42a5caaf5d47
concaveNP <concave@acm.org>
2016-05-01 17:39:37 -0600

    Created the backend module and added Joke support to it.  Additionally move the dependency from the app module to the backend module for jokelib use.

---
+++ backend/src/main/java/com/concavenp/nanodegree/backend/MyJoke.java
@@ -0,0 +1,18 @@
+package com.concavenp.nanodegree.backend;
+
+/**
+ * Created by dave on 5/1/16.
+ */
+public class MyJoke {
+
+    private String joke;
+
+    public String getJoke() {
+        return joke;
+    }
+
+    public void setJoke(String joke) {
+        this.joke = joke;
+    }
+
+}
