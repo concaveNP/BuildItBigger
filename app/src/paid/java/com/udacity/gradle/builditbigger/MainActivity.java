c75b0d6daefdec25c61620f1b4901e511e41e2db
dave <concaveNP@gmail.com>
2016-06-18 22:40:06 -0600

    Removed the Settings menu because there aren't any.

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -18,14 +18,16 @@ public class MainActivity extends AppCompatActivity{
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
 
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-
-    }
+// There is currently no need to display the settings icon - there are not any to show
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
